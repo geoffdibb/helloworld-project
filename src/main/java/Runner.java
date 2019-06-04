612ca459b59338f4759b72fa19b1056c076c2d28
Marcofdoom <marcvpartington@gmail.com>
2019-06-04 14:45:23 +0100

    first comment

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Runner {
+public static void main(String[] args) {
+	
+	helloWorldPrinter printOut = new helloWorldPrinter();
+	
+	printOut.print();
+}
+}
---
+++ src/main/java/helloWorldPrinter.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class helloWorldPrinter {
+
+	public void print() {
+		System.out.println("Hellow world to the standard out");
+	}
+}
